512f6de7df5731579698a441d7fbbe91d23c5e88
Tommyyard <tommxcy@gmail.com>
2024-11-04 20:37:56 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const tombol1 = document.getElementById("btn1");
+const tombol2 = document.getElementById("btn2");
+const fb = document.getElementById("feedb");
+
+window.onload = function() {
+  alert(`Website ini masih dalam tahap pengembangan,
+    jadi masih banyak fitur yang belum bisa dijalankan!`);
+}
+
+tombol1.addEventListener("click", function () {
+  window.location.href = "masuk.html";
+});
+
+tombol2.addEventListener("click", function () {
+  window.location.href = "info-keanggotaan.html";
+});
+
+function toggleMenu(event) {
+  const menuContent = document.getElementById('menuContent');
+  menuContent.classList.toggle('show');
+  event.stopPropagation(); // Prevent click event from bubbling up
+}
+
+window.onclick = function(event) {
+  const menuContent = document.getElementById("menuContent");
+  const menuIcon = document.querySelector(".menu-icon");
+  // Close the menu if the click is outside the menu content and icon
+  if (!menuContent.contains(event.target) && !menuIcon.contains(event.target)) {
+      menuContent.classList.remove('show'); // Close menu if clicking outside
+  }
+}
